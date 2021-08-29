205d7106826f28837c38bfe7dfe2a100075e38e9
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-08-29 13:23:55 +0300

    initialize commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ProfileService } from './profile.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import 'rxjs/add/operator/catch';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ProfileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
