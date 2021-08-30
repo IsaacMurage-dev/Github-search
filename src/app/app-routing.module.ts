b7cd5a8dccb17b1d77f20868460b408c4d5ed8f9
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-08-30 20:53:57 +0300

    generate search and navbar class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent },
+  {path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import 'rxjs/add/operator/catch';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    NavbarComponent,
+    SearchComponent,
     
   ],
   imports: [
