48ae8d64e5f271851d6e9d7889cec680153cf965
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-08-31 15:18:57 +0300

    modify changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,26 +7,9 @@ import { ProfileService } from '../../app/profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profile:any;
-  repos:any;
-  username:string;
 
-  constructor(private profileService: ProfileService) {
-    
-  }
 
-  findProfile(){
-    this.profileService.updateProfile(this.username);
-    this.profileService.getProfileInfo().subscribe(profile => {
-      console.log(profile);
-      this.profile = profile;
-    });
-
-    this.profileService.getProfileRepos().subscribe(repos => {
-      console.log(repos);
-      this.repos = repos;
-    })
-  }
+  
 
   ngOnInit() {
   }
